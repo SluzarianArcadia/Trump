29140faed2b19fcf6ad0499198cc61fbb18902e5
SluzarianArcadia <ericslusarz@gmail.com>
2023-02-03 21:50:04 +1100

    Built Dropdown List of Unique Targets

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const NUMBER_OF_TWEETS = 10360; 
 var JSONTweets = [];
+var JSONTargets = [];
 
 $.getJSON("TrumpInsults.json", function(json){
     JSONTweets = json;
+});
+
+$.getJSON("uniqueTargets.json", function(json){
+    JSONTargets = json.targetsName;
+    console.log(JSONTargets)
+    renderSelectDropDown(JSONTargets)
 });
 
 $("#randomBtn").click(function (e) { 
@@ -12,3 +19,12 @@ $("#randomBtn").click(function (e) {
     $('#Insult').text(tweetPicked.insult).hide().fadeToggle(600);
     $('#Tweet') .text(tweetPicked.tweet) .hide().fadeToggle(800);
 });
+
+
+function renderSelectDropDown(JSONTargets){
+    JSONTargets.forEach((element, i) => {
+        var container = $('<option />');
+        container.html(element).appendTo()
+        $(container).appendTo("#listOfTargets");
+    });
+}
